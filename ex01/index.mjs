2a927b132c8b77f90b2bbd7878d972fd11a3d8b5
= <=>
2022-11-08 17:39:34 +0100

    rock paper scissors frontend

--- ex01/index.mjs
+++ ex01/index.mjs
@@ -1,3 +1,4 @@
+//rock paper scissors with Tamara and Petar
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
@@ -8,9 +9,18 @@ const [ accTamara, accPetar ] =
   await stdlib.newTestAccounts(2, startingBalance);
 console.log('Hello, Tamara and Petar!');
 
-console.log('Launching...');
-const ctcTamara = accTamara.contract(backend);
-const ctcPetar = accPetar.contract(backend, ctcTamara.getInfo());
+console.log('Launching Rock Paper Scissors...');
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['Tamara wins', 'Draw', 'Petar wins'];
+const Player = (Who) =>({
+  getHand:()=>{
+    const hand_num = Math.floor(Math.random()*3) //gives us 0 1 2, so we can know what did they choose in HAND array
+    console.log(`${Who} played ${HAND[hand_num]}`);
+  },
+  seeOutcome:(outcome)=>{
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`)
+  }
+})
 
 console.log('Starting backends...');
 await Promise.all([
