162113d7938764b38c5c8c121896988c11b51d6e
= <=>
2022-11-08 17:11:46 +0100

    testing ./reach init command

--- ex01/build/index.main.mjs
+++ ex01/build/index.main.mjs
@@ -21,7 +21,6 @@ export function _getViews(s, viewlib) {
     infos: {
       },
     views: {
-      1: []
       }
     };
   
@@ -42,49 +41,7 @@ export async function Alice(ctcTop, interact) {
   const stdlib = ctc.stdlib;
   
   
-  const txn1 = await (ctc.sendrecv({
-    args: [],
-    evt_cnt: 0,
-    funcNum: 0,
-    lct: stdlib.checkedBigNumberify('./index.rsh:12:5:dot', stdlib.UInt_max, '0'),
-    onlyIf: true,
-    out_tys: [],
-    pay: [stdlib.checkedBigNumberify('./index.rsh:12:5:decimal', stdlib.UInt_max, '0'), []],
-    sim_p: (async (txn1) => {
-      const sim_r = { txns: [], mapRefs: [], maps: [] };
-      let sim_txn_ctr = stdlib.UInt_max;
-      const getSimTokCtr = () => { sim_txn_ctr = sim_txn_ctr.sub(1); return sim_txn_ctr; };
-      
-      
-      const {data: [], secs: v39, time: v38, didSend: v22, from: v37 } = txn1;
-      
-      ;
-      sim_r.isHalt = false;
-      
-      return sim_r;
-      }),
-    soloSend: true,
-    timeoutAt: undefined /* mto */,
-    tys: [],
-    waitIfNotPresent: false
-    }));
-  const {data: [], secs: v39, time: v38, didSend: v22, from: v37 } = txn1;
-  ;
-  const txn2 = await (ctc.recv({
-    didSend: false,
-    evt_cnt: 0,
-    funcNum: 1,
-    out_tys: [],
-    timeoutAt: undefined /* mto */,
-    waitIfNotPresent: false
-    }));
-  const {data: [], secs: v42, time: v41, didSend: v26, from: v40 } = txn2;
-  ;
   return;
-  
-  
-  
-  
   };
 export async function Bob(ctcTop, interact) {
   if (typeof(ctcTop) !== 'object' || ctcTop._initialize === undefined) {
@@ -95,53 +52,7 @@ export async function Bob(ctcTop, interact) {
   const stdlib = ctc.stdlib;
   
   
-  const txn1 = await (ctc.recv({
-    didSend: false,
-    evt_cnt: 0,
-    funcNum: 0,
-    out_tys: [],
-    timeoutAt: undefined /* mto */,
-    waitIfNotPresent: false
-    }));
-  const {data: [], secs: v39, time: v38, didSend: v22, from: v37 } = txn1;
-  ;
-  const txn2 = await (ctc.sendrecv({
-    args: [],
-    evt_cnt: 0,
-    funcNum: 1,
-    lct: v38,
-    onlyIf: true,
-    out_tys: [],
-    pay: [stdlib.checkedBigNumberify('./index.rsh:15:5:decimal', stdlib.UInt_max, '0'), []],
-    sim_p: (async (txn2) => {
-      const sim_r = { txns: [], mapRefs: [], maps: [] };
-      let sim_txn_ctr = stdlib.UInt_max;
-      const getSimTokCtr = () => { sim_txn_ctr = sim_txn_ctr.sub(1); return sim_txn_ctr; };
-      
-      
-      const {data: [], secs: v42, time: v41, didSend: v26, from: v40 } = txn2;
-      
-      ;
-      sim_r.txns.push({
-        kind: 'halt',
-        tok: undefined /* Nothing */
-        })
-      sim_r.isHalt = true;
-      
-      return sim_r;
-      }),
-    soloSend: true,
-    timeoutAt: undefined /* mto */,
-    tys: [],
-    waitIfNotPresent: false
-    }));
-  const {data: [], secs: v42, time: v41, didSend: v26, from: v40 } = txn2;
-  ;
   return;
-  
-  
-  
-  
   };
 const _ALGO = {
   ABI: {
@@ -149,7 +60,7 @@ const _ALGO = {
     pure: [],
     sigs: []
     },
-  appApproval: `ByACAAEmAQAiNQAxGEEAxChkSSJbNQGBCFs1AjYaABdJQQAHIjUEIzUGADYaAhc1BDYaAzYaARdJIwxAAB0jEkQjNAESRDQESSISTDQCEhFEgASai5F0sEIAKUiBoI0GiACOIjQBEkQ0BEkiEkw0AhIRRIAEXw2r+rAjNQEyBjUCQgAcMRmBBRJEsSKyASKyCCOyEDIJsgkyCrIHs0IABTEZIhJEKDQBFjQCFlBnNAZBAAqABBUffHU0B1CwNABJIwgyBBJEMRYSRCNDMRkiEkRC/98iMTQSRCMxNRJEIjE2EkQiMTcSRCI1ASI1AkL/rzQASUojCDUAOAcyChJEOBAjEkQ4CBJEiQ==`,
+  appApproval: `ByACAAEmAQAiNQAxGEEAeShkSSJbNQGBCFs1AjYaABdJQQAHIjUEIzUGADYaAhc1BDYaAzYaARcAMRmBBRJEsSKyASKyCCOyEDIJsgkyCrIHs0IABTEZIhJEKDQBFjQCFlBnNAZBAAqABBUffHU0B1CwNABJIwgyBBJEMRYSRCNDMRkiEkRC/98iMTQSRCMxNRJEIjE2EkQiMTcSRCI1ASI1AkL/rw==`,
   appClear: `Bw==`,
   companionInfo: null,
   extraPages: 0,
@@ -163,102 +74,6 @@ const _ALGO = {
   };
 const _ETH = {
   ABI: `[
-  {
-    "inputs": [
-      {
-        "components": [
-          {
-            "internalType": "uint256",
-            "name": "time",
-            "type": "uint256"
-          },
-          {
-            "internalType": "bool",
-            "name": "msg",
-            "type": "bool"
-          }
-        ],
-        "internalType": "struct T1",
-        "name": "_a",
-        "type": "tuple"
-      }
-    ],
-    "stateMutability": "payable",
-    "type": "constructor"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "msg",
-        "type": "uint256"
-      }
-    ],
-    "name": "ReachError",
-    "type": "error"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "internalType": "address",
-        "name": "_who",
-        "type": "address"
-      },
-      {
-        "components": [
-          {
-            "internalType": "uint256",
-            "name": "time",
-            "type": "uint256"
-          },
-          {
-            "internalType": "bool",
-            "name": "msg",
-            "type": "bool"
-          }
-        ],
-        "indexed": false,
-        "internalType": "struct T1",
-        "name": "_a",
-        "type": "tuple"
-      }
-    ],
-    "name": "_reach_e0",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "internalType": "address",
-        "name": "_who",
-        "type": "address"
-      },
-      {
-        "components": [
-          {
-            "internalType": "uint256",
-            "name": "time",
-            "type": "uint256"
-          },
-          {
-            "internalType": "bool",
-            "name": "msg",
-            "type": "bool"
-          }
-        ],
-        "indexed": false,
-        "internalType": "struct T1",
-        "name": "_a",
-        "type": "tuple"
-      }
-    ],
-    "name": "_reach_e1",
-    "type": "event"
-  },
   {
     "stateMutability": "payable",
     "type": "fallback"
@@ -308,55 +123,18 @@ const _ETH = {
     "type": "function"
   },
   {
-    "inputs": [
-      {
-        "components": [
-          {
-            "internalType": "uint256",
-            "name": "time",
-            "type": "uint256"
-          },
-          {
-            "internalType": "bool",
-            "name": "msg",
-            "type": "bool"
-          }
-        ],
-        "internalType": "struct T1",
-        "name": "_a",
-        "type": "tuple"
-      }
-    ],
-    "name": "_reach_m1",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
     "stateMutability": "payable",
     "type": "receive"
   }
 ]`,
-  Bytecode: `0x60806040526040516106773803806106778339810160408190526100229161017d565b600080554360035560408051338152825160208083019190915283015115158183015290517faf3102e4a96b239a2811210526ca19adcde1af3e2876a2c9a5886a5a887a2dcb9181900360600190a161007d341560076100bb565b600160008181554390915560408051602080820184905282518083038201815291830190925280516100b39260029201906100e4565b505050610220565b816100e05760405163100960cb60e01b81526004810182905260240160405180910390fd5b5050565b8280546100f0906101e5565b90600052602060002090601f0160209004810192826101125760008555610158565b82601f1061012b57805160ff1916838001178555610158565b82800160010185558215610158579182015b8281111561015857825182559160200191906001019061013d565b50610164929150610168565b5090565b5b808211156101645760008155600101610169565b60006040828403121561018f57600080fd5b604080519081016001600160401b03811182821017156101bf57634e487b7160e01b600052604160045260246000fd5b60405282518152602083015180151581146101d957600080fd5b60208201529392505050565b600181811c908216806101f957607f821691505b6020821081141561021a57634e487b7160e01b600052602260045260246000fd5b50919050565b6104488061022f6000396000f3fe6080604052600436106100405760003560e01c80631e93b0f1146100495780632c10a1591461006d5780638323075714610080578063ab53f2c61461009557005b3661004757005b005b34801561005557600080fd5b506003545b6040519081526020015b60405180910390f35b61004761007b3660046102fe565b6100b8565b34801561008c57600080fd5b5060015461005a565b3480156100a157600080fd5b506100aa6101e6565b604051610064929190610316565b6100c86001600054146009610283565b6100e2813515806100db57506001548235145b600a610283565b6000808055600280546100f490610373565b80601f016020809104026020016040519081016040528092919081815260200182805461012090610373565b801561016d5780601f106101425761010080835404028352916020019161016d565b820191906000526020600020905b81548152906001019060200180831161015057829003601f168201915b505050505080602001905181019061018591906103b6565b90507f400d21ea4e4a5e28b4ae5f0f476c201fc8036473fcf7c8cd252f38698020b4f133836040516101b89291906103da565b60405180910390a16101cc34156008610283565b600080805560018190556101e2906002906102a8565b5050565b6000606060005460028080546101fb90610373565b80601f016020809104026020016040519081016040528092919081815260200182805461022790610373565b80156102745780601f1061024957610100808354040283529160200191610274565b820191906000526020600020905b81548152906001019060200180831161025757829003601f168201915b50505050509050915091509091565b816101e25760405163100960cb60e01b81526004810182905260240160405180910390fd5b5080546102b490610373565b6000825580601f106102c4575050565b601f0160209004906000526020600020908101906102e291906102e5565b50565b5b808211156102fa57600081556001016102e6565b5090565b60006040828403121561031057600080fd5b50919050565b82815260006020604081840152835180604085015260005b8181101561034a5785810183015185820160600152820161032e565b8181111561035c576000606083870101525b50601f01601f191692909201606001949350505050565b600181811c9082168061038757607f821691505b6020821081141561031057634e487b7160e01b600052602260045260246000fd5b80151581146102e257600080fd5b6000602082840312156103c857600080fd5b81516103d3816103a8565b9392505050565b6001600160a01b038316815281356020808301919091526060820190830135610402816103a8565b801515604084015250939250505056fea26469706673582212202e51cc506887c340f2fd6bfabb3faa581c748cfb261c1d637c806dc49310b0c964736f6c634300080c0033`,
-  BytecodeLen: 1655,
+  Bytecode: `0x608060405234801561001057600080fd5b50610205806100206000396000f3fe6080604052600436106100355760003560e01c80631e93b0f11461003e5780638323075714610062578063ab53f2c61461007757005b3661003c57005b005b34801561004a57600080fd5b506003545b6040519081526020015b60405180910390f35b34801561006e57600080fd5b5060015461004f565b34801561008357600080fd5b5061008c61009a565b604051610059929190610137565b6000606060005460028080546100af90610194565b80601f01602080910402602001604051908101604052809291908181526020018280546100db90610194565b80156101285780601f106100fd57610100808354040283529160200191610128565b820191906000526020600020905b81548152906001019060200180831161010b57829003601f168201915b50505050509050915091509091565b82815260006020604081840152835180604085015260005b8181101561016b5785810183015185820160600152820161014f565b8181111561017d576000606083870101525b50601f01601f191692909201606001949350505050565b600181811c908216806101a857607f821691505b602082108114156101c957634e487b7160e01b600052602260045260246000fd5b5091905056fea2646970667358221220ac639294bfee7bddfd240259ee8e367fdc4f3bbabd83fc2ce4ad132138b60c4f64736f6c634300080c0033`,
+  BytecodeLen: 549,
   Which: `oD`,
   version: 8,
   views: {
     }
   };
 export const _stateSourceMap = {
-  1: {
-    at: './index.rsh:13:11:after expr stmt semicolon',
-    fs: [],
-    msg: null,
-    who: 'Module'
-    },
-  2: {
-    at: './index.rsh:16:11:after expr stmt semicolon',
-    fs: [],
-    msg: null,
-    who: 'Module'
-    }
   };
 export const _Connectors = {
   ALGO: _ALGO,
--- ex01/index.mjs
+++ ex01/index.mjs
@@ -1,22 +1,27 @@
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
-const stdlib = loadStdlib();
+const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 
-const accAlice = await stdlib.newTestAccounts(startingBalance);
-const accBob = await stdlib.newTestAccounts(startingBalance);
-  
+const [ accAlice, accBob ] =
+  await stdlib.newTestAccounts(2, startingBalance);
+console.log('Hello, Alice and Bob!');
+
+console.log('Launching...');
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
+console.log('Starting backends...');
 await Promise.all([
-  ctcAlice.p.Alice({
-    //Alice code
+  backend.Alice(ctcAlice, {
+    ...stdlib.hasRandom,
+    // implement Alice's interact object here
   }),
-  ctcBob.p.Bob({
-    //Bob code
-  })
+  backend.Bob(ctcBob, {
+    ...stdlib.hasRandom,
+    // implement Bob's interact object here
+  }),
 ]);
 
 console.log('Goodbye, Alice and Bob!');
